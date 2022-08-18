6c7e5f578a1f934223e9c3ab8ed8c56f193da135
kenichi miyata <miyataken999@bpmboxes.com>
2022-08-18 23:31:33 +0900

    Update app.py

--- src/app.py
+++ src/app.py
@@ -14,6 +14,7 @@ class HelloParam(BaseModel):
 
 @app.get("/hello")
 def get_hello(name: str = None):
+    print("test")
     """
     getで返事するsssssssssssssssssssssssssssssssssssssssssssssssssss
     """
